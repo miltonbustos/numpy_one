af713075de47deb22dae3705c94cba16e6ae4266
Milton_Bustos <miltonbustos8416@gmail.com>
2025-11-08 21:26:18 -0500

    initial commit

---
+++ image_rotator.py
@@ -0,0 +1,15 @@
+# image_rotator.py
+
+from PIL import Image # importa el módulo Image de la librería PIL
+
+# carga una imagen llamada 'tripleten_logo.jpeg.png'.
+im = Image.open('tripleten_logo.jpeg.png')
+
+# obtén el tamaño de la imagen mediante el atributo .size y muéstralo
+print(im.size)
+
+# gira la imagen 90 grados en sentido contrario a las agujas del reloj
+rotated = im.rotate(90)
+
+# guarda la imagen girada
+rotated.save('rotated.png')
